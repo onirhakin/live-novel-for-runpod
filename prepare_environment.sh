0b977e449c0985220bf8c5f90de5d49f789cc6ab
On Irhakin <>
2024-02-27 15:15:41 +0100

    Switching to LLama CPP (changed prepare environment to install minimal libraries)

--- prepare_environment.sh
+++ prepare_environment.sh
@@ -2,19 +2,26 @@
 
 LIVE_NOVEL_ROOT=$(pwd)
 
-cd "$LIVE_NOVEL_ROOT/backend"
+#cd "$LIVE_NOVEL_ROOT/backend"
 # removes blinker, as seems that pip is not able to remove it, and that would prevent the installation of flask
 
-apt-get -y remove python3-blinker
+#apt-get -y remove python3-blinker
 # install requirements
-pip install -r requirements.txt
+#pip install -r requirements.txt
 
 #isntall tensorRT
-pip install tensorrt --extra-index-url https://pypi.nvidia.com
+#pip install tensorrt --extra-index-url https://pypi.nvidia.com
 # install requirements for torch, because is strange and want them in a particular way
 #python -m pip install -r torch_requirements.txt
 # install stuff that must be installed later other wise overwritten (maybe)
 #python -m pip install -r requirements2.txt
+
+# download,compile and install llama-cpp-python with GPU enabled (cuda 11.8)
+CUDACXX="/usr/local/cuda-11.8/bin/nvcc" CMAKE_ARGS="-DLLAMA_CUBLAS=on -DCMAKE_CUDA_ARCHITECTURES=all-major" FORCE_CMAKE=1 pip install llama-cpp-python --no-cache-dir --force-reinstall --upgrade
+
+# install flask for making the server
+pip install flask
+pip install flask-cors
 
 cd "$LIVE_NOVEL_ROOT/backend/models"
 # dowloads the llm model to execute
