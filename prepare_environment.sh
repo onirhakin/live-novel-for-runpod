73abcaa72d1d17fbfd0d55e5b853ddb1adc90764
On Irhakin <>
2024-02-28 01:05:49 +0100

    Removed old requierments.

--- prepare_environment.sh
+++ prepare_environment.sh
@@ -2,18 +2,8 @@
 
 LIVE_NOVEL_ROOT=$(pwd)
 
-#cd "$LIVE_NOVEL_ROOT/backend"
 # removes blinker, as seems that pip is not able to remove it, and that would prevent the installation of flask
 apt-get -y remove python3-blinker
-# install requirements
-#pip install -r requirements.txt
-
-#isntall tensorRT
-#pip install tensorrt --extra-index-url https://pypi.nvidia.com
-# install requirements for torch, because is strange and want them in a particular way
-#python -m pip install -r torch_requirements.txt
-# install stuff that must be installed later other wise overwritten (maybe)
-#python -m pip install -r requirements2.txt
 
 # inatall transformers for tokenizer to measure token length
 pip install transformers
